4e3c23c133e3cae19cde5bf60f27f703666e561e
Sundar S <mail2sundarsaravanan@gmail.com>
2025-12-29 16:35:04 +0530

    .sh file added

--- Dockerfile
+++ Dockerfile
@@ -12,7 +12,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # Default port for Uvicorn
-EXPOSE 8000
+EXPOSE 10000
 
 # Launch your FastAPI app
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "10000"]
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from pipeline.run import run_fest_scrape
 
 run_fest_scrape(
-    fest_url="https://techkriti.org/sponsors",
-    fest_name="Techkriti",
-    college="IIT Kanpur",
-    year=2025,
+    fest_url="https://sponsors.springfest.in/",
+    fest_name="Spring Fest",
+    college="IIT KGP",
+    year=2026,
 )
